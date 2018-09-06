f5542ccd70aa3100dff8c507bc6a85b1c956f00c
frgen <31874180+frgen@users.noreply.github.com>
2018-09-06 16:15:26 -0300

    Add files via upload

---
+++ TP1/UTN_INPUT.h
@@ -0,0 +1,59 @@
+/** \brief Permite ingresar un numero y mostrar error en caso de no serlo
+ *
+ * \param float Almacena la entrada del usuario
+ * \param char texto[] Mensaje para que el usuario ingrese un numero
+ * \param char textoError[] Mensaje de error al ingresar algo distinto a un numero
+ * \return float Devuelve el numero ingresado
+ *
+ */
+float pedirNumero(float, char texto[], char textoError[]);
+
+
+/** \brief Calcula la suma de los operandos ingresados
+ *
+ * \param float Almacena el primer operando
+ * \param float Almacena el segundo operando
+ * \return float Devuelve la suma calculada
+ *
+ */
+float calcularSuma(float, float);
+
+
+/** \brief Calcula la resta de los operandos ingresados
+ *
+ * \param float Almacena el primer operando
+ * \param float Almacena el segundo operando
+ * \return float Devuelve la resta calculada
+ *
+ */
+float calcularResta(float, float);
+
+
+/** \brief Calcula la multiplicacion de los operandos ingresados
+ *
+ * \param float Almacena el primer operando
+ * \param float Almacena el segundo operando
+ * \return float Devuelve la multiplicacion calculada
+ *
+ */
+float calcularMultiplicacion(float, float);
+
+
+/** \brief Calcula la division de los operandos ingresados
+ *
+ * \param float Almacena el primer operando
+ * \param float Almacena el segundo operando
+ * \return float Devuelve la division calculada
+ *
+ */
+float calcularDivision(float, float);
+
+
+/** \brief Calcula el factorial de los operandos ingresados
+ *
+ * \param float Almacena el operando
+ * \return int Devuelve el factorial calculado
+ *
+ */
+int calcularFactorial(float);
+
